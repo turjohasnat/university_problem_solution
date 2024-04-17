ce5783296a8ee5ac4f67238a5353f57c45b06d22
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-04-17 10:10:49 +0600

    Create Problem-05.c

---
+++ Programming language C/01. Intro/Code/Problem-05.c
@@ -0,0 +1,16 @@
+/*Task - Program that will do the followings:
+a)	Declare a variable uninitialized
+b)	Declare and  initialize a variable in one statement
+c)	Declare and initialize multiple variables with different values in one statement
+d)	Declare and initialize multiple variables with the same value in one statement */
+
+//C Solution:
+#include <stdio.h>
+
+int main(){
+    int var1;
+    int var2=10;
+    int var_1=5, var_2=10, var_3=15, var_4=20;
+    int var_a=10, var_b=10, var_c=10, var_d=10;
+    return 0;
+}
