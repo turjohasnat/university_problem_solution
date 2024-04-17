f1245bf3388d732c2ee08e0982691201afd63444
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-04-17 10:39:11 +0600

    Create Problem-12.c

---
+++ Programming language C/01. Intro/Code/Problem-12.c
@@ -0,0 +1,18 @@
+//Task - Program that will defineaconstant using “CONST” and print the value.
+
+//Sample output:
+//The value of pi: 3.14
+//The value of golden ratio: 1.62
+
+//C Solution:
+#include<stdio.h>
+
+int main(){
+    const float pi=3.14;
+    const float goldenRatio=1.62;
+
+    printf("The value of pi:%.2f\n",pi);
+    printf("The value of golden ratio:%.2f\n",goldenRatio);
+
+    return 0;
+}
