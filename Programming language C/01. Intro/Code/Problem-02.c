bbdc15944325dc06641b225d7e1c5f1d086c6aed
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-04-17 10:17:31 +0600

    Update Problem-02.c

--- Programming language C/01. Intro/Code/Problem-02.c
+++ Programming language C/01. Intro/Code/Problem-02.c
@@ -1,3 +1,12 @@
+// Task - Program that will use newline/tab and print the following segment:
+
+//Sample output:
+/* 
+Hello World.
+This is my first program.           C is fun. 
+*/
+
+//C Solution:
 #include <stdio.h>
     int main (){
         printf("Hello World.\nThis is my first program.\tC is fun.");
