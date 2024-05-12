9f2866609ce6203fac81e88cf6dd2ff29f6d2ffe
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-12 20:31:03 +0600

    Create Problem-03.c

---
+++ Programming language C/03. Conditions/Code/Problem-03.c
@@ -0,0 +1,51 @@
+//Task - Program that will take an integer of length one from the terminal and then display the digit in English.
+
+//Sample input:
+// 9
+// 0
+
+//Sample output:
+// nine 
+// Zero
+
+//C Solution:
+#include<stdio.h>
+
+int main(){
+    int digit;
+    printf("Enter your number:");
+    scanf("%d",&digit);
+
+    if(digit==0){
+        printf("Zero");
+    }
+    else if (digit==1){
+        printf("One");
+    }
+    else if (digit==2){
+        printf("Two");
+    }
+    else if(digit==3){
+        printf("Three");
+    }
+    else if (digit==4){
+        printf("Four");
+    }
+    else if (digit==5){
+        printf("Five");
+    }
+    else if (digit==6){
+        printf("Six");
+    }
+    else if (digit==7){
+        printf("Seven");
+    }
+    else if(digit==8){
+        printf("Eight");
+    }
+    else if (digit==9){
+        printf("Nine");
+    }
+    
+    return 0;
+}
