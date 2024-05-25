eff3c52a814ed9f84b1fefb6b841afcb13354637
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-25 23:52:01 +0600

    Update Problem-08.c

--- Programming language C/03. Conditions/Code/Problem-08.c
+++ Programming language C/03. Conditions/Code/Problem-08.c
@@ -1,17 +1,3 @@
-//Task - Program that will decide whether a year is leap year or not.
-
-// Yes, if ( Year % 4 == 0 && year % 100 != 0 )   ||  ( Year % 400 ==0 )
-
-//Sample input:
-// 2000
-// 2004
-// 2014
-
-//Sample output:
-// Yes
-// Yes
-// No
-
 //C Solution:
 #include<stdio.h>
 
