358772bad13cd4bf89021f849a3cffb55a540383
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-25 23:55:13 +0600

    Update Problem-06.c

--- Programming language C/01. Intro/Code/Problem-06.c
+++ Programming language C/01. Intro/Code/Problem-06.c
@@ -1,12 +1,3 @@
-//Task - Program that will take your age in year(s)as input and print it.
-
-//Sample input:
-// 20, 21.
-
-//Sample output:
-// My age is: 20, My age is: 21.
-
-
 //C Solution:
 #include <stdio.h>
 
