23e9bfaa74b27cb2560d8d43c0b9a86e47635cd7
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-25 23:57:44 +0600

    Update Problem-14.c

--- Programming language C/01. Intro/Code/Problem-14.c
+++ Programming language C/01. Intro/Code/Problem-14.c
@@ -1,13 +1,3 @@
-/* Task - Program that will define a global and a local variable with the same name but with different values, and then do the following steps in order-
-A.	Print the value of the variable before defining the local variable
-B.	Print the value of the variable after defining the local variable
-C.	Explicitly print the value of the variable as global */
-
-/* Sample output:
- A.	Global: 10
- B.	Local: 20
- C.	Global: 10 */
-
 //C Solution:
 #include <stdio.h>
 
