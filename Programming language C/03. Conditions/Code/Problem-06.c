f05e0e242fdc7b6c042860488e48e7ad5b08740d
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-25 23:51:05 +0600

    Update Problem-06.c

--- Programming language C/03. Conditions/Code/Problem-06.c
+++ Programming language C/03. Conditions/Code/Problem-06.c
@@ -1,17 +1,3 @@
-//Task - Program that will read from the console a random number and check if it is a nonzero positive number.If the check is yes, it will determine if the number is a power of 2.
-
-// If the check fails the program will check for two more cases. If the number is zero, the program will print “Zero is not a valid input”. Else it will print “Negative input is not valid”.
-
-//Sample input:
-// 0, 1, 512, 1022, -512
-
-//Sample output:
-// Zero is not a valid input
-// Yes
-// Yes
-// No
-// Negative input is not valid
-
 //C Solution:
 #include<stdio.h>
 #include<math.h>
