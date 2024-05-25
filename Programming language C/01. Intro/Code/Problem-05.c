095cbc114c65a094547489f9ab8be3369a84f9e2
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-25 23:54:53 +0600

    Update Problem-05.c

--- Programming language C/01. Intro/Code/Problem-05.c
+++ Programming language C/01. Intro/Code/Problem-05.c
@@ -1,9 +1,3 @@
-/*Task - Program that will do the followings:
-a)	Declare a variable uninitialized
-b)	Declare and  initialize a variable in one statement
-c)	Declare and initialize multiple variables with different values in one statement
-d)	Declare and initialize multiple variables with the same value in one statement */
-
 //C Solution:
 #include <stdio.h>
 
