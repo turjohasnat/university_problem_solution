845d86c44e71c3a09f25d8a0e03d3cbc91d12a63
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-21 22:53:26 +0600

    Update Problem-15.c

--- Programming language C/02 Operators /Code/Problem-15.c
+++ Programming language C/02 Operators /Code/Problem-15.c
@@ -1,11 +1,3 @@
-//Task - Program to find size of int, float, double and char of the system.
-
-//Sample Output:
-// Size of int in byte(s) = 4
-// Size of float in byte(s) = 4
-// Size of double in byte(s) = 8
-// Size of char in byte(s) = 1
-
 //C Solution:
 #include <stdio.h>
 
