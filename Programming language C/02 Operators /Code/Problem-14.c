0892f8060cc85ccf60e4c458d70354b08f9a458f
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-21 22:54:44 +0600

    Update Problem-14.c

--- Programming language C/02 Operators /Code/Problem-14.c
+++ Programming language C/02 Operators /Code/Problem-14.c
@@ -1,14 +1,3 @@
-//Task - Program that will take a floating point number X as input and evaluate A,B,C where-
-// A = Value when X is rounded up to the nearest integer
-// B = Value when X is rounded down to the nearest integer
-// C = Absolute value of X
-
-//Sample input(X):
-// X = 10.6
-
-//Sample output:
-//A = 11, B = 10, C = 10.6
-
 //C Solution:
 #include <stdio.h>
 #include <math.h>
