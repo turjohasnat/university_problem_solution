a970d30ba89f1530244e8e7d1d3298d96a48af13
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-21 23:12:19 +0600

    Update Problem-11.c

--- Programming language C/03. Conditions/Code/Problem-11.c
+++ Programming language C/03. Conditions/Code/Problem-11.c
@@ -1,18 +1,3 @@
-//Task - Program that will take the final score of a student in a particular subject as input and find his/her grade.
-// Marks  ||	Letter Grade ||	Marks	  || Letter Grade	|| Marks	      || Letter Grade
-// 90-100	||  A 	         || 70-73	  ||   C+ 	      || Less than 55 ||	F
-// 86-89	||  A- 	         || 66-69	  ||   C 		      || 82-85	      ||  B+ 	
-// 62-65	||  C-		       || 78-81 	||   B 	        || 58-61	      ||  D+ 		
-// 74-77	||  B- 	         || 55-57	  ||    D 		
-
-//Sample input:
-// 91.5
-// 50
-
-//Sample output:
-// Grade: A
-// Grade: F
-
 //C Solution:
 #include<stdio.h>
 
