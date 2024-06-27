775db33d862ac17c4f6d24600e8c6e6bcfdaa654
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-27 11:34:29 +0600

    Update Problem-13.c

--- Programming language C/02 Operators /Code/Problem-13.c
+++ Programming language C/02 Operators /Code/Problem-13.c
@@ -1,13 +1,16 @@
 //C Solution:
+//C Solution:
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    int x;
-    printf("Enter your value:");
-    scanf("%d",&x);
-    float y;
-    y = 2*pow(cos(x),2)-sqrt(3)*sin(x)+sin(x/2);
-    printf("Result:%f\n",y);
+int main() {
+    double x;
+    printf("Enter the value of x (1 <= x <= 180): ");
+    scanf("%lf", &x);
+
+    double x_radians= x * M_PI / 180.0;
+    double result = 2 * pow(cos(x_radians), 2) - sqrt(3) * sin(x_radians) + log10(x / 2);
+    printf("%f\n", result);
+    
     return 0;
 }
