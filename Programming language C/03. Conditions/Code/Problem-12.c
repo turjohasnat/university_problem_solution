e18e171be2fe364d2e64837ef3775a6464a0f01d
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-27 22:31:33 +0600

    Update Problem-12.c

--- Programming language C/03. Conditions/Code/Problem-12.c
+++ Programming language C/03. Conditions/Code/Problem-12.c
@@ -1,15 +1,3 @@
-//Task - Program that will construct a menu for performing arithmetic operations. The user will give two real numbers (a,b) on which the arithmetic operations will be performed and an integer number (1<= Choice<=4)asa choice. Choice-1, 2, 3, 4 are for performing addition, subtraction, multiplication, division (quotient) respectively.
-
-// Sample input (a,b,Choice):
-// 5     10
-// 3
-// -5     10
-// 4
-
-//Sample output:
-// Multiplication: 50
-// Quotient: 0
-
 //C Solution:
 #include<stdio.h>
 
