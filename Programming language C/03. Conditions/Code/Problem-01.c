1efb0599124ca7f9698504af852501ae46f7977e
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-06-30 10:57:04 +0600

    Add files via upload

---
+++ Programming language C/03. Conditions/Code/Problem-01.c
@@ -0,0 +1,15 @@
+//C Solution:
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    printf("Addition:%d\n", x+y);
+    printf("Subtraction:%d\n", x-y);
+    printf("Multiplication:%d\n", x*y);
+    printf("Quotient:%d\n", x/y);
+    printf("Reminder:%d\n", x%y);
+
+    return 0;
+}
