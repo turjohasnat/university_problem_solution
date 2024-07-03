68f060c616b148b90784643b32cb7d38a50f798c
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-03 14:08:08 +0600

    Update Problem-03.c

--- Programming language C/04.Loops/Problem-03.c
+++ Programming language C/04.Loops/Problem-03.c
@@ -2,18 +2,12 @@
 #include<stdio.h>
 
 int main(){
-    int i, n;
+    int n;
     scanf("%d", &n);
-    for ( i = 1; i <= n; i++ ){
-        if ( i % 2 == 0 ){
-            printf("0");
-        }
-        else{
-            printf("1");
-        }
-        if ( i < n ){
-            printf(", ");
-        }
+    for ( int i = 1; i <= n; i++){
+        if ( i % 2 == 0 ) printf("0");
+        else printf("1");
+        if ( i < n ) printf(",");
     }
     return 0;
 }
