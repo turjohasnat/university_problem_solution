8df113cabb8268a401fad2391b2e3b70226bdef2
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-03 14:05:00 +0600

    Update Problem-02.c

--- Programming language C/04.Loops/Problem-02.c
+++ Programming language C/04.Loops/Problem-02.c
@@ -2,13 +2,11 @@
 #include<stdio.h>
 
 int main(){
-    int i, n;
+    int n;
     scanf("%d", &n);
-    for( i = 1; i <= n ; i++){
-        printf("%d", 2*i - 1);
-        if ( i < n ){
-            printf(", ");
-        }
+    for ( int i = 1; i <= n; i++){
+        printf("%d", 2*i-1);
+        if ( i < n ) printf(",");
     }
     return 0;
 }
