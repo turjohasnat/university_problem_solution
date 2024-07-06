25e80b476aba0091521ebb2e4324c22d2be038c1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-06 12:26:37 +0600

    Update Problem-12.c

--- Programming language C/04.Loops/Problem-12.c
+++ Programming language C/04.Loops/Problem-12.c
@@ -3,13 +3,13 @@
 int main(){
     int n;
     scanf("%d", &n);
-    int frist_number = 1, second_number = 1;
-    printf("%d,%d", frist_number, second_number); 
-    for ( int i = 3; i <= n; i++ ){
-        int next_number = frist_number + second_number;
-        printf(",%d", next_number);
-        frist_number = second_number;
-        second_number = next_number;
+    int first_num = 1, second_num = 1;
+    printf("%d,%d", first_num, second_num );
+    for ( int i = 3; i <= n; i++){
+        int next_num = first_num + second_num;
+        printf(",%d", next_num);
+        first_num = second_num;
+        second_num = next_num;
     }
     return 0;
 }
