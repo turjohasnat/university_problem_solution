e543cd6eb8469a1ddd62cd7a2fcf1ee987d99fd9
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-06 14:03:55 +0600

    Update Problem-13.c

--- Programming language C/04.Loops/Problem-13.c
+++ Programming language C/04.Loops/Problem-13.c
@@ -3,16 +3,16 @@
 int main(){
     int n;
     scanf("%d", &n);
-    printf("%d!= ", n);
+    printf("%d! = ",n);
     int fact = 1;
-    for ( int i = n; i > 0; i-- ){
-        fact*=i;
+    for ( int i = n; i >= 1; i-- ){
+        fact *= i;
         if ( i < n ){
             printf(" X ");
         }
-        printf("%d",i);
+        printf ("%d", i);
     }
-    printf(" = %d\n",fact);
+    printf(" = %d\n", fact);
 
     return 0;
 }
