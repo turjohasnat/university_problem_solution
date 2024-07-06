e057b84e8b3456b2d6f43885a2ce1600b8d12979
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-06 12:05:17 +0600

    Update Problem-11.c

--- Programming language C/04.Loops/Problem-11.c
+++ Programming language C/04.Loops/Problem-11.c
@@ -6,7 +6,7 @@ int main(){
     int n;
     scanf("%d", &n);
     int result = 0;
-    for ( int i = 1; i <= n; i++ ){
+    for ( int i = 1; i <= n; i++){
         int sum = pow(i,2)*(i+1);
         result += sum;
     }
