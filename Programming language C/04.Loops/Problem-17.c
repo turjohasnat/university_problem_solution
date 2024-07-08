8f616bc84485498b9846c404d0b2c0990ddaaa90
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-08 19:33:24 +0600

    Update Problem-17.c

--- Programming language C/04.Loops/Problem-17.c
+++ Programming language C/04.Loops/Problem-17.c
@@ -3,23 +3,21 @@
 int main(){
     int n;
     scanf("%d", &n);
-
     int prime = 1;
-    if ( n <= 1 ){
+    if ( n < 2 ){
         prime = 0;
     }
-    else {
-        for ( int i = 2; i < n; i++){
+    else{
+        for ( int i = 2; i < n; i++ ){
             if ( n % i == 0 ){
                 prime = 0;
                 break;
             }
         }
     }
-    if ( prime ){
-        printf ("Prime\n");
-    }
-    else printf ("Not prime\n");
+    if ( prime ) printf("prime\n");
+    else printf("Not prime\n");
+    
 
     return 0;
 
