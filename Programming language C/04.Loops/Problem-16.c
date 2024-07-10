0037519df6290992cbd5a6e7f9de6ca90b1d6d18
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-10 22:59:38 +0600

    Update Problem-16.c

--- Programming language C/04.Loops/Problem-16.c
+++ Programming language C/04.Loops/Problem-16.c
@@ -5,12 +5,12 @@ int main(){
     scanf("%d %d", &num1, &num2);
 
     int gcd, lcm;
-    for ( int i = 1; i <= num1 && i <= num2; i++){
-        if ( num1 % i == 0 && num2 % i == 0){
+    for ( int i = 1; i <= num1 && i <= num2; i++ ){
+        if ( num1 % i == 0 && num2 % i == 0 ){
             gcd = i;
         }
     }
-    lcm = (num1*num2)/gcd;
+    lcm = num1 * num2 / gcd;
     printf("GCD: %d\n", gcd);
     printf("LCM: %d\n", lcm);
 
