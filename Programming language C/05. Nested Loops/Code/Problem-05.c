c12e361597bcf288e8197891352ad4bdf2b323b6
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-11 10:54:05 +0600

    Add files via upload

---
+++ Programming language C/05. Nested Loops/Code/Problem-05.c
@@ -0,0 +1,13 @@
+//C Solution:
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for ( int i = 1; i <= n; i++ ){
+        for ( int j = n; j >= n-i+1; j-- ){
+            printf("%d", j );
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Programming language C/05. Nested Loops/Code/Problem-09.c
@@ -0,0 +1,15 @@
+//C Solution:
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for ( int i = 1; i <= n; i++ ){
+        for ( int j = 1; j <= n; j++ ){
+            if ( (i+j)%2 == 0 ) printf("1");
+            else printf("0");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
