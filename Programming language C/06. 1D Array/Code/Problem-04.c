889dfb70a98217973c29f73fc4a3fc876279efe5
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-12 20:01:06 +0600

    Add files via upload

---
+++ Programming language C/06. 1D Array/Code/Problem-04.c
@@ -0,0 +1,15 @@
+//C Solution:
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    int array[n];
+    for ( int i = 0; i < n; i++ ){
+        scanf("%d", &array[i] );
+        if ( i % 2 == 0 ) sum += array[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
