7e1b25120273e83a6ebe550e165dd18b8831db5a
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-12 19:32:32 +0600

    Create Problem-03.c

---
+++ Programming language C/06. 1D Array/Code/Problem-03.c
@@ -0,0 +1,16 @@
+//C Solution:
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    int array[n];
+    for ( int i = 0; i < n; i++ ){
+        scanf("%d", &array[i] );
+        if ( array[i] % 2 == 0 ){
+            sum += array[i];
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
