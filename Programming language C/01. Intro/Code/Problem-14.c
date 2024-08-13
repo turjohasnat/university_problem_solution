bf0eda64b5f34a3adfe4a5a234fb51dfd583de8b
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-08-13 19:32:35 +0600

    Update Problem-14.c

--- Programming language C/01. Intro/Code/Problem-14.c
+++ Programming language C/01. Intro/Code/Problem-14.c
@@ -1,14 +1,13 @@
 //C Solution:
 #include <stdio.h>
-
-int variable=10;
-int main(){
-    int *ptr=&variable;
-    printf("A. Global:%d\n",variable);
-
-    int variable=20;
-    printf("B. Local:%d\n",variable);
-    printf("C. Global:%d\n", *ptr);
-
+int global = 10;
+int main() {
+    printf("A. Global: %d\n", global);
+    int global = 20;
+    printf("B. Local: %d\n", global);
+    {
+        extern int global;
+        printf("C. Global: %d\n", global);
+    }
     return 0;
 }
