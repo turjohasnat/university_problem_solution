27e0a68ea2a8fb4dcbab52b8804a18aee28a7b00
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-08-29 22:44:11 +0600

    Add files via upload

---
+++ Programming language C/05. Nested Loops/Code/Problem-20.c
@@ -0,0 +1,15 @@
+//C Solution:
+#include <stdio.h>
+    int main() {
+    int n;
+    scanf("%d", &n);
+
+    int num = 0,sum = 0;
+    for (int i = 1; i <= n; i++) {
+        num =num * 10 + i;
+        sum += num;
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
