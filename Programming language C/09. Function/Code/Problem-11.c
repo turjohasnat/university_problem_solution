b117cc9ace994027f2d444a71e3a32e55c4f7963
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-04 10:33:36 +0600

    Create Problem-11.c

---
+++ Programming language C/09. Function/Code/Problem-11.c
@@ -0,0 +1,22 @@
+//C Solution:
+#include<stdio.h>
+
+int isPrime( int n );
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(isPrime(n)) printf("Prime\n");
+    else printf("Not Prime\n");
+    return 0;
+}
+
+int isPrime( int n ){
+    if( n < 2 ) return 0;
+    else{
+        for(int i = 2; i < n-1; i++ ){
+            if( n % i == 0 ) return 0;
+        }
+        return 1;
+    }
+}
