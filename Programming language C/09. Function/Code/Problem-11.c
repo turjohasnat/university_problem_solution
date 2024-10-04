37b1be4797a4832058c3664faa7fa715fb41f72c
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-04 10:55:42 +0600

    Update Problem-11.c

--- Programming language C/09. Function/Code/Problem-11.c
+++ Programming language C/09. Function/Code/Problem-11.c
@@ -14,7 +14,7 @@ int main(){
 int isPrime( int n ){
     if( n < 2 ) return 0;
     else{
-        for(int i = 2; i < n-1; i++ ){
+        for(int i = 2; i <= n/2; i++ ){
             if( n % i == 0 ) return 0;
         }
         return 1;
