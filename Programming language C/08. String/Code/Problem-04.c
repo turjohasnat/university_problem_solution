14e8c29743636def71eecd8082e2b98ca237f1e3
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-12 22:16:33 +0600

    Create Problem-04.c

---
+++ Programming language C/08. String/Code/Problem-04.c
@@ -0,0 +1,13 @@
+//C Solution:
+#include<stdio.h>
+int main(){
+    char string[123];
+    gets(string);
+
+    int length = 0;
+    while ( length[string] != 0 ){
+        length++;
+    }
+    printf("%d", length);
+    return 0;
+}
