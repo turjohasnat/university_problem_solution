bd4a26b4dc465f218a87eaab9515270ac2b4adb6
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-18 20:19:51 +0600

    Update Problem-08.c

--- Programming language C/08. String/Code/Problem-08.c
+++ Programming language C/08. String/Code/Problem-08.c
@@ -2,15 +2,30 @@
 #include<stdio.h>
 
 int main(){
-    char string_1[1023], string_2[1023];
+    char string_1[100], string_2[100];
     gets(string_1);
     gets(string_2);
 
-    int result = strcmp(string_1, string_2);
+    int i = 0;
+    while( string_1[i] != 0 && string_2[i] != 0 ){
+        if ( string_1[i] < string_2[i] ){
+            printf("%s\n", string_1);
+            return 0;
+        }
+        else if ( string_1[i] > string_2[i] ){
+            printf("%s\n", string_2);
+            return 0;
+        }
+        i++;
+    }
 
-    if ( result < 0 ) printf("%s", string_1);
-    else if ( result > 0 ) printf("%s", string_2);
-    else printf("Both equal");
+    if( string_1[i] != 0 && string_2[i] != 0 ){
+        printf("Both equal\n");
+    }
+    else if( string_1[i] != 0 ){
+        printf("%s\n", string_2);
+    }
+    else printf("%s\n", string_1);
 
     return 0;
 }
