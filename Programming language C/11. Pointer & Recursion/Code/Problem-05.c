203d604cd2dc53154953659a9493fa507c7cd7aa
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-19 22:26:00 +0600

    Create Problem-05.c

---
+++ Programming language C/11. Pointer & Recursion/Code/Problem-05.c
@@ -0,0 +1,19 @@
+//C Solution:
+#include <stdio.h>
+
+void swap ( int *a, int *b );
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    swap( &a, &b );
+    printf("%d %d", a, b);
+    return 0;
+}
+
+void swap ( int *a, int *b ){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
