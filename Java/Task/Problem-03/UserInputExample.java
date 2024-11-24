ef04bb9c8d35ed8d010e472222d18528f26770bc
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-11-24 21:23:34 +0600

    Create UserInputExample.java

---
+++ Java/Task/Problem-03/UserInputExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInputExample {
+    public static void main( String[] args ){
+        Scanner scanner = new Scanner( System.in );
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello " + name + ", Your age is " + age + ".");
+    }
+}
