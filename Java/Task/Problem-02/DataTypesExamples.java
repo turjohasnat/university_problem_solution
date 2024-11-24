e3ed91cf0cd539e480d726edd15bafeff14c889d
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-11-24 21:14:58 +0600

    Create DataTypesExamples.java

---
+++ Java/Task/Problem-02/DataTypesExamples.java
@@ -0,0 +1,15 @@
+public class DataTypesExamples{
+    public static void main( String[] args){
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isPassed = true;
+        float bonus = 2550.50f;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+        System.out.println("Bonus: " + bonus);
+    }
+}
