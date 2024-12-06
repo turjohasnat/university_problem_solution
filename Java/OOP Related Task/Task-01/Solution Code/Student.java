e925b4715bb4eb0cf5aadcf42832a660c0e54bd1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-06 22:14:08 +0600

    Add files via upload

---
+++ Java/OOP Related Task/Task-01/Solution Code/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    // Properties
+    String name; // Attribute
+    int id; // Attribute
+    double cgpa; // Attribute
+
+    // Methods
+    public void studentDetails(){
+        System.out.println("Name: " + name );
+        System.out.println("ID: " + id );
+        System.out.println("CGPA: " + cgpa);
+    }
+}
