8c526aa0824846eec128888d43d951925ffe1863
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-07 22:04:48 +0600

    Create Solution.java

---
+++ Java/Practice_Problems/Problem-04/Solution.java
@@ -0,0 +1,8 @@
+//Java Solution:
+public class Main {
+    public static void main(String[] args) {
+        double result = (9.5 * 4.5) - (2.5 * 3);
+        result /= (45.5-3.5);
+        System.out.println("Result: " + result);
+    }
+}
