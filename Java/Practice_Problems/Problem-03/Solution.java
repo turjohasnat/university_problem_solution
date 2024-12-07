4a49ce878a18c2c6b44844c09d8bd8e3a6b4a8e1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-07 21:55:55 +0600

    Create Solution.java

---
+++ Java/Practice_Problems/Problem-03/Solution.java
@@ -0,0 +1,9 @@
+//Java Solution:
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a  a^2  a^3");
+        for( int i = 1; i < 4; i++ ){
+            System.out.println(i + "   " + (i*i) + "    " + (i*i*i));
+        }
+    }
+}
