45317e37b3a82c15838697f692afaa3b0b75736c
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-09 22:39:05 +0600

    Create Problem-04.cpp

---
+++ C++/01. Linear Search/Code/Problem-04.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            bool isDuplicate = false;
+            for(int j = 0; j < nums.size(); j++){
+                if(i != j && nums[i] == nums[j]){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(isDuplicate == false)
+                return nums[i];
+        }
+        return -1;
+    }
+};
