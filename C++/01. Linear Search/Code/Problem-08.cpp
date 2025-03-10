e0320dda1b154346e9491bdecf0ec972277af815
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-10 23:02:51 +0600

    Update Problem-08.cpp

--- C++/01. Linear Search/Code/Problem-08.cpp
+++ C++/01. Linear Search/Code/Problem-08.cpp
@@ -1,1 +1,21 @@
+//C++ Solution:
 
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for(int i = 0; i < arr.size(); i++){
+            freq[arr[i]]++;
+        }
+
+        unordered_set<int> unique_num;
+        for(auto u : freq){
+            unique_num.insert(u.second);
+        }
+
+        if(freq.size() == unique_num.size())
+            return true;
+        else 
+            return false;
+    }
+};
