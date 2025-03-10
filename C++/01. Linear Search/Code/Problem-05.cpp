05ebdfc0999caba0316a02e14664bd6e0abbefc1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-10 16:13:04 +0600

    Create Problem-05.cpp

---
+++ C++/01. Linear Search/Code/Problem-05.cpp
@@ -0,0 +1,22 @@
+//C++ Solution:
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++){
+            int index = abs(nums[i]) - 1;
+
+            if(nums[index] > 0){
+                nums[index] *= -1;
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0)
+                result.push_back(i+1);
+        }
+        return result;
+    }
+};
