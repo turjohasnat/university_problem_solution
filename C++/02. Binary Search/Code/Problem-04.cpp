e13c2d3ed672bd64a48f871489ebee5f35f5540d
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-06 21:07:11 +0600

    Create Problem-04.cpp

---
+++ C++/02. Binary Search/Code/Problem-04.cpp
@@ -0,0 +1,21 @@
+//C++ Solution:
+
+//Runtime: 0 ms
+//Memory Usage: 12.5 MB
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int lb = 0, ub = nums.size() - 1;
+
+        while(lb < ub){
+            int mid = (lb + ub)/2;
+            
+            if(nums[mid] >= nums[mid+1])
+                ub = mid;
+            else
+                lb = mid + 1;
+        }
+        return lb;
+    }
+};
