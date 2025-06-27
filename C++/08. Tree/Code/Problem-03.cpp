34b8728940183b4c8d75703bf6d0b9ac06946a97
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-06-27 21:21:18 +0600

    Create Problem-03.cpp

---
+++ C++/08. Tree/Code/Problem-03.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> nums;
+        inorder(root, nums);
+        
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+    
+private:
+    void inorder(TreeNode* node, vector<int>& nums) {
+        if (!node) return;
+        inorder(node->left, nums);
+        nums.push_back(node->val);
+        inorder(node->right, nums);
+    }
+};
